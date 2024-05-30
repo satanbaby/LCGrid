de3c8ddc1826c894bc798965ea71a584444c5100
satanbaby <joe8605273512882@gmail.com>
2024-05-30 22:26:27 +0800

    允許傳入url

--- src/components/LcDropdown/LcDropdown.js
+++ src/components/LcDropdown/LcDropdown.js
@@ -1,16 +1,48 @@
+const {
+  ref,
+  onMounted,
+  onBeforeUnmount
+} = Vue
 export default {
   components: {
     dropdown: primevue.dropdown
   },
-  setup(props, { emit }) {
-    
+  props:{
+    queryUrl: String
+  },
+  setup(props) {
+    const dataSource = ref([])
+    onMounted(()=>{
+      console.log(props.queryUrl)
+      if(props.queryUrl){
+        // get data from ajax
+        const source = []
+        setTimeout(() => {
+          for (let i = 0; i < 10; i++) {
+            source.push({Text: `選項 ${i}`, Value: i})
+          }
+          dataSource.value = source
+        }, 1000);
+      }
+    })
+    // onBeforeUnmount(()=>{
+    //   console.log('onBeforeUnmount')
+    // })
+      return {
+        queryUrl: props.queryUrl,
+        dataSource
+      }
   },
   template: `
-  <dropdown 
+  <dropdown
     append-to="self" 
     placeholder="請選擇" 
-    filter 
+    :options= dataSource
+    option-label="Text"
+    option-value="Value"
+    filter
     show-clear
-    ></dropdown>
+    >
+    </dropdown>
   `,
 }
